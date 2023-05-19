3250f43f377860c88e00b74c4d7c4f09552880c2
samu <samueljose.garcia@iesdonana.org>
2023-05-19 16:06:53 +0200

    Funcionando la búsqueda por países

--- main.py
+++ main.py
@@ -18,12 +18,24 @@ with open('data.csv') as csvfile:
     # Recojo la información en forma de diccionario.
     reader = csv.DictReader(csvfile)
 
-    # Recorro el diccionario accediendo por las claves correspondientes a la población por años.
+    dict_population = {}
+
     for row in reader:
-        population_data = [(row['Country'], ((2022, row['2022 Population']), (2020, row['2020 Population']), (2015, row['2015 Population']), (2010, row['2010 Population']), (2000, row['2000 Population']), (1990, row['1990 Population']), (1980, row['1980 Population']), (1970, row['1970 Population'])))]
-        # print(population_data)
-        dict_population = {dict(population_data)}
-        print(dict_population)
+        population_data = [
+                            (2022, row['2022 Population']),
+                            (2020, row['2020 Population']),
+                            (2015, row['2015 Population']),
+                            (2010, row['2010 Population']),
+                            (2000, row['2000 Population']),
+                            (1990, row['1990 Population']),
+                            (1980, row['1980 Population']),
+                            (1970, row['1970 Population'])
+                            ]
+
+        dict_population[row['Country']] = [(year, row[f'{year} Population']) for year in [2022, 2020, 2015, 2010, 2000, 1990, 1980, 1970]]  # usar lista por descomprensión?
+
+    print(dict_population['Jordan'])
+
 
 
 # Creación de gráficos.
